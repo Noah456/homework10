77fb7b83d736fabe3adf6159d2dd970b5ed2bf59
Noah Aflalo <noahaflalo3@gmail.com>
2020-07-15 00:01:07 -0400

    final commit

---
+++ Develop/lib/Engineer.js
@@ -0,0 +1,20 @@
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer {
+    constructor(email,name,id, github) {
+        this.getId = () => {return id}
+        this.getRole = () => { return "Engineer"};
+        this.getName = () => {return name}
+        this.getEmail = () => {return email};
+        this.getGithub = () => {return github};
+    }
+
+    getRole() {
+        return "Engineer";
+      }
+    
+      getGithub() {
+        return this.github;
+      }
+}
+
+module.exports = {Engineer};
